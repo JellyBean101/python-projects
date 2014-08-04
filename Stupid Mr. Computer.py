776a540fc4e5adf69176b37400647061dee3af68
JellyBean101 <Paquettepack@gmail.com>
2014-08-04 17:34:46 -0400

    I Hate My Stupid Computer

---
+++ Stupid Mr. Computer.py
@@ -0,0 +1,14 @@
+print("ttucvtayugtttt")
+
+print("the butt:stampy says I HATE SPIDERS")
+
+print("there anoying")
+
+
+answer = input("how is a mouse so small?")
+print("You answered: " + answer)
+
+if answer == "someone stepped on it":
+    print("Correct!")
+else:
+    print("False!")
