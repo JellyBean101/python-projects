a046f38d1be56579d09eff176ded19a1ea5eccc2
JellyBean101 <Paquettepack@gmail.com>
2014-08-11 18:35:56 -0400

    i did letter p.o.o.p

---
+++ im rich.py
@@ -0,0 +1,13 @@
+print("chaching$")
+print("lalalalalalalalalalalallolololololololol")
+print(">:)")
+print("im gonna go greif those people over there")
+print(">:B")
+print("(:/")
+import random
+import time
+while True:
+    letters = "p.o.o.p"
+    letter = random.choice(letters)
+    print(letter)
+    time.sleep(1)
